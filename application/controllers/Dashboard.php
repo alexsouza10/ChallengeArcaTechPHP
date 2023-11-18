5731e3ad70865c29e2f962a4d9422ebd51743de3
alexsouza10 <alexsouza160298@gmail.com>
2023-11-18 14:13:53 -0300

    feat: dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,7 +1,15 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Dashboard extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		permission();
+		$this->load->model('games_model');
+
+	}
 	public function index()
 	{
 		$this->load->model('games_model');

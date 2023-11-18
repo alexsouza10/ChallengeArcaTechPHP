21062b1862afafa1ccf6c8b55cd82aef4b8f43b0
alexsouza10 <alexsouza160298@gmail.com>
2023-11-18 13:09:54 -0300

    feat: Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -11,8 +11,21 @@ class Login extends CI_Controller
 	}
 	public function store()
 	{
+		$this->load->model('login_model');
 		$email = $_POST["email"];
-		$password = $_POST["password"];
+		$password = md5($_POST["password"]);
 		$user = $this->login_model->store($email, $password);
+
+		if($user) {
+			$this->session->set_userdata("logged_user", $user);
+			redirect("dashboard");
+		}else {
+			redirect("login");
+		}
+	}
+
+	public function logout() {
+		$this->session->unset_userdata("logged_user");
+		redirect("login");
 	}
 }

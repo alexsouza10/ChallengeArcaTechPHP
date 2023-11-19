11282c61f699a80b9b75b32b5d41b0171874ca77
alexsouza10 <alexsouza160298@gmail.com>
2023-11-19 12:58:22 -0300

    feat: buscaCep

--- application/views/pages/viacep.php
+++ application/views/pages/viacep.php
@@ -9,23 +9,26 @@
 </head>
 
 <body>
-	<form action="<?= base_url('index.php/cep/consulta') ?>" method="post">
+	<form action="<?= base_url('/') ?>" method="post">
 		<p>Digite o CEP para encontrar o endereço.</p>
 		<input type="text" placeholder="Digite um cep..." name="cep" value="<?= $address->cep ?>">
-		
-
+</br>
 		<label for="rua">Rua:</label>
 		<input type="text" placeholder="Rua" name="rua" value="<?= $address->logradouro ?>">
-
+		</br>
 		<label for="bairro">Bairro:</label>
 		<input type="text" placeholder="Bairro" name="bairro" value="<?= $address->bairro ?>">
-
+		</br>
 		<label for="cidade">Cidade:</label>
 		<input type="text" placeholder="Cidade" name="cidade" value="<?= $address->localidade ?>">
-
+		</br>
 		<label for="estado">Estado:</label>
 		<input type="text" placeholder="Estado" name="estado" value="<?= $address->uf ?>">
+		</br>
 		<input type="submit" value="Return">
+		</br>
+		<form action="<?= base_url('/') ?>" method="post">
+		<input type="submit" value="Return"><span></span>
 	</form>
 </body>
 
